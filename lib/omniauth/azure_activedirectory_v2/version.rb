f50609a8f384e248838d172796b03edcc4b3ffca
Gerard Montemayor <me@grdmnt.com>
2022-09-26 12:49:34 +0800

    Bump version to 2.1.0

--- lib/omniauth/azure_activedirectory_v2/version.rb
+++ lib/omniauth/azure_activedirectory_v2/version.rb
@@ -2,8 +2,8 @@ module Omniauth
   module Azure
     module Activedirectory
       module V2
-        VERSION = "2.0.0"
-        DATE    = "2022-09-14"
+        VERSION = "2.1.0"
+        DATE    = "2022-09-26"
       end
     end
   end
